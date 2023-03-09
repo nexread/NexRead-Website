3e49f2e7beb9308151433fe4fca31938f16181c6
sahilarora3117 <iamsahil@protonmail.com>
2023-03-09 13:52:57 +0530

    fixed struct

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Navigation } from '@/components/Navigation'
+import { Header } from '@/components/Header'
+import { Feature } from '@/components/Feature'
+
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <Feature />
+    </>
+  )
+}
