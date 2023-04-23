8d4229e9470ecef3a0d058092d54667ea5a8c516
sahilarora3117 <iamsahil@protonmail.com>
2023-04-23 11:26:46 +0530

    added .nojekyll

--- next.config.js
+++ next.config.js
@@ -2,21 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (true) {
-  // trim off `<owner>/`
-  const repo = "NexRead-Website"
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
 module.exports = nextConfig
