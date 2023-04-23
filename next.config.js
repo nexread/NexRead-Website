996ea52d17efa960916390aa8b4020e68113c3b8
sahilarora3117 <iamsahil@protonmail.com>
2023-04-23 11:13:52 +0530

    added a attmep to fix github actions deploy for images

--- next.config.js
+++ next.config.js
@@ -2,5 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
 module.exports = nextConfig
