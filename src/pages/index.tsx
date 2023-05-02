a86ba7f03153df228eb6a3e3fb1687d5ddbc1346
sahilarora3117 <iamsahil@protonmail.com>
2023-05-02 12:48:24 +0530

    added title and meta tags for title and description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { Navigation } from '@/components/Navigation'
 import { Header } from '@/components/Header'
 import { Feature } from '@/components/Feature'
-
+import Head from 'next/head'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Nexread - Nexread Team</title>
+        <meta property="og:title" content="Nexread - Nexread Team" key="title" />
+        <meta name="og:description" content="NexRead is a news aggregator app that enables you to stay up-to-date with the latest headlines, all from within your own Nextcloud account. With NexRead, you can customize your news feed by selecting your favorite news sources, topics, and regions of interest."/>
+      </Head>
       <Navigation />
       <Header />
       <Feature />
